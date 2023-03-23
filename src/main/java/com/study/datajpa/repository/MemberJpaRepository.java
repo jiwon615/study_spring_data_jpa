f72df1b881df79f0d8e057c1155e10d2395b2138
jiwon615 <seg615@naver.com>
2023-03-23 14:40:05 +0900

    feat: 순수 JPA 기반 repository CRUD 및 테스트코드

--- src/main/java/com/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberJpaRepository.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
 
-    @PersistenceContext
+    @PersistenceContext  // Jpa에서 EntityManager를 injection (주입) 해주는 애노테이션
     private EntityManager em;
 
     public Member save(Member member) {
@@ -17,7 +19,27 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        // JPQL 사용 (테이블 대상이 아닌 객체를 대상으로 한 조회)
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList(); // 리스트 반환
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult(); // 단건 반환
     }
 }
--- src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,11 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional // jpa의 모든 변경은 트랜잭션 안에서 이루어져야 하므로, Transactional 필요
-@Rollback(false) // transaction 끝나면 결과 롤백해버리므로, 디비에 값을 확인해볼 수 없어서 테스트에서는 공부를 위해 rollback(false)
+//@Rollback(false) // transaction 끝나면 결과 롤백해버리므로, 디비에 값을 확인해볼 수 없어서 테스트에서는 공부를 위해 rollback(false)
 class MemberRepositoryTest {
 
     @Autowired
@@ -26,5 +28,32 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member); // 같은 트랜잭션 안에서는 영속성 컨텍스트 안에서의 인스턴스는 동일성을 보장
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
