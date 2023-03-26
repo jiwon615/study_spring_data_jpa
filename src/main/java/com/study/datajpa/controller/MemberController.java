9aadbbe622ffb9988d8b31c1d21de843c4741ee0
jiwon615 <seg615@naver.com>
2023-03-26 12:48:42 +0900

    feat: 도메인클래스 컨버터 적용

---
+++ src/main/java/com/study/datajpa/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.study.datajpa.controller;
+
+import com.study.datajpa.entity.Member;
+import com.study.datajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    // 도메인 클래스 컨버터 적용 전
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    // 도메인 클래스 컨버터 적용 후 (repository 이용안했는데 위에 findById()과 동일쿼리 나감)
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("user1"));
+    }
+}
